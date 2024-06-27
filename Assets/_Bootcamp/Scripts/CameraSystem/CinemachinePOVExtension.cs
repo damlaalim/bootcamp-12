582094af9997d8933bc8051b5e74e6d6fbe25124
damlaalim <damlaalim81@gmail.com>
2024-06-27 18:01:42 +0300

    cinemachine extension güncellendi

--- Assets/_Bootcamp/Scripts/CameraSystem/CinemachinePOVExtension.cs
+++ Assets/_Bootcamp/Scripts/CameraSystem/CinemachinePOVExtension.cs
@@ -7,24 +7,38 @@ namespace _Bootcamp.Scripts.CameraSystem
 {
     public class CinemachinePovExtension : CinemachineExtension
     {
-        [SerializeField] 
-        private float _clampAngle = 80, _horizontalSpeed = 10, _verticalSpeed = 10;
+        [SerializeField] private float _clampAngle = 80, _horizontalSpeed = 10, _verticalSpeed = 10;
 
         [Inject] private PlayerInputController _inputController;
-        
+
         private Vector3 _startingRot;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _startingRot = transform.localRotation.eulerAngles;
+        }
 
         protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
             if (!vcam.Follow || stage != CinemachineCore.Stage.Aim || _inputController is null) return;
-            if (_startingRot == null)
-                _startingRot = transform.localRotation.eulerAngles;
-            
+
             var deltaInput = _inputController.GetLookDelta();
-            _startingRot.x += deltaInput.x * _verticalSpeed   * Time.deltaTime;
-            _startingRot.y += deltaInput.y * _horizontalSpeed * Time.deltaTime;
+        
+            _startingRot.x += deltaInput.x * _horizontalSpeed * deltaTime;
+            _startingRot.y += deltaInput.y * _verticalSpeed * deltaTime;
+
+            _startingRot.x = NormalizeAngle(_startingRot.x);
             _startingRot.y = Mathf.Clamp(_startingRot.y, -_clampAngle, _clampAngle);
+            
             state.RawOrientation = Quaternion.Euler(-_startingRot.y, _startingRot.x, 0f);
+        }
+
+        private float NormalizeAngle(float angle)
+        {
+            while (angle > 360f) angle -= 360f;
+            while (angle < 0f) angle += 360f;
+            return angle;
         }
     }
 }
