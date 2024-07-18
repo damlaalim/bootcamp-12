313ff7f5ad7e52af68e5ed6243db6cc8a6995761
Burak Arslan <81029405+Burak-droid@users.noreply.github.com>
2024-07-19 01:41:10 +0300

    Multiplayer Tamamlandı
    Optimizasyonları yapıldı

---
+++ Assets/_Bootcamp/Scripts/LobbyCode/GameFramework/Network/Movement/InputState.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+namespace _Bootcamp.Scripts.LobbyCode.GameFramework.Network.Movement
+{
+    public class InputState
+    {
+        public int Tick;
+        public Vector2 MovementInput;
+        public Vector2 LookInput;
+        
+
+    }
+}
