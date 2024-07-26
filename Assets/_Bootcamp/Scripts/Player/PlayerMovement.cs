71a16305a414dceb86e74f66378f24a297c1f6f0
damlaalim <damlaalim81@gmail.com>
2024-07-26 03:37:01 +0300

    karakter animasyonları eklendi ve düzeltildi

--- Assets/_Bootcamp/Scripts/Player/PlayerMovement.cs
+++ Assets/_Bootcamp/Scripts/Player/PlayerMovement.cs
@@ -13,7 +13,7 @@ namespace _Bootcamp.Scripts.Player
         
         private PlayerInputController _playerInput;
         private CharacterController _characterController;
-        private bool _groundedPlayer, _isMove;
+        private bool _groundedPlayer, _isMove, _isJump, _isIdle, _isRun;
         private Vector3 _playerVelocity;
         private Transform _cameraTransform;
 
@@ -23,44 +23,79 @@ namespace _Bootcamp.Scripts.Player
             _characterController = GetComponent<CharacterController>();
 
             _cameraTransform = Camera.main.transform;
-            canMove = true;
+            canMove = _isIdle = true;
         }
 
         private void Update()
         {
             Move();
+
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                _anim.CrossFade("dance", .2f);
+                _isMove = _isRun = _isIdle =  false;
+            }
         }
 
         private void Move()
         {
-            if (!canMove)
-            {
-                if (_isMove)
-                    _anim.CrossFade("Idle", .1f);
-                _isMove = false;
-                return;
-            }
-
-            if (!_isMove)
-            {
-                _isMove = true;
-                _anim.CrossFade("Walk", .1f);
-            }
-            
             _groundedPlayer = _characterController.isGrounded;
+            if (_groundedPlayer && _isJump)
+                _isJump = false;
+            
             if (_groundedPlayer && _playerVelocity.y < 0) _playerVelocity.y = 0f;
 
             var moveInput = _playerInput.GetMovement();
+            var sprinted = _playerInput.SprintedThisFrame();
             var move = (_cameraTransform.forward * moveInput.y + _cameraTransform.right * moveInput.x).With(y: 0);
-            var sprintMultiplier = _playerInput.SprintedThisFrame() ? _sprintMultiplier : 1;
+            var sprintMultiplier = sprinted ? _sprintMultiplier : 1;
             
             _characterController.Move(move * (Time.deltaTime * _moveSpeed * sprintMultiplier));
-            
-            if (_playerInput.JumpedThisFrame() && _groundedPlayer) 
+
+            if (_playerInput.JumpedThisFrame() && _groundedPlayer)
+            {
+                if (!_isJump)
+                {
+                    _isJump = true;
+                    _isRun = _isIdle = _isMove = false;
+                    _anim.CrossFade("Jump", 0f);
+                }
                 _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+            }
 
             _playerVelocity.y += _gravityValue * Time.deltaTime;
             _characterController.Move(_playerVelocity * Time.deltaTime);
+            
+            if (moveInput == Vector2.zero)
+            {
+                if (!_isIdle && (_isMove || _isRun) && !_isJump)
+                {
+                    _anim.CrossFade("Idle", .2f);
+                    _isIdle = true;
+                }
+                _isMove = _isRun = false;
+                return;
+            }
+
+            if (moveInput != Vector2.zero && !_isJump)
+            {
+                _anim.SetFloat("WalkSpeed", moveInput.y < 0 ? -1 : 1);
+
+                if (sprinted && !_isRun)
+                {
+                    _isRun = true;
+                    _anim.CrossFade("Run", .1f);
+                    _isMove = false;
+                }
+                else if (!sprinted && !_isMove)
+                {
+                    _isMove = true;
+                    _anim.CrossFade("Walk", .1f);
+                    _isRun = false;
+                }
+                
+                _isIdle = false;
+            }
         }
     }
 }
