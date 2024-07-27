bf7050c8a3e064b93478b4876904a836df72dc9d
damlaalim <damlaalim81@gmail.com>
2024-07-27 23:45:19 +0300

    karakterin rigleri değiştirildi ve modeli yenilendi
    yeni animasyonlar eklendi

--- Assets/_Bootcamp/Scripts/Player/PlayerMovement.cs
+++ Assets/_Bootcamp/Scripts/Player/PlayerMovement.cs
@@ -8,12 +8,12 @@ namespace _Bootcamp.Scripts.Player
     public class PlayerMovement : MonoBehaviour
     {
         public bool canMove;
-        [SerializeField] private float _sprintMultiplier, _moveSpeed, _gravityValue = -9.81f, _jumpHeight = 1.0f;
+        [SerializeField] private float _sprintMultiplier, _moveSpeed, _gravityValue = -9.81f, _jumpHeight = 6.0f;
         [SerializeField] private Animator _anim;
         
         private PlayerInputController _playerInput;
         private CharacterController _characterController;
-        private bool _groundedPlayer, _isMove, _isJump, _isIdle, _isRun;
+        private bool _groundedPlayer, _isMove, _isJump, _isIdle, _isRun, _isDance;
         private Vector3 _playerVelocity;
         private Transform _cameraTransform;
 
@@ -28,21 +28,14 @@ namespace _Bootcamp.Scripts.Player
 
         private void Update()
         {
+            Actions();
             Move();
-
-            if (Input.GetKeyDown(KeyCode.F1))
-            {
-                _anim.CrossFade("dance", .2f);
-                _isMove = _isRun = _isIdle =  false;
-            }
         }
 
         private void Move()
         {
             _groundedPlayer = _characterController.isGrounded;
-            if (_groundedPlayer && _isJump)
-                _isJump = false;
-            
+
             if (_groundedPlayer && _playerVelocity.y < 0) _playerVelocity.y = 0f;
 
             var moveInput = _playerInput.GetMovement();
@@ -56,9 +49,9 @@ namespace _Bootcamp.Scripts.Player
             {
                 if (!_isJump)
                 {
+                    _anim.CrossFade("Jump", .1f);
                     _isJump = true;
-                    _isRun = _isIdle = _isMove = false;
-                    _anim.CrossFade("Jump", 0f);
+                    _isRun = _isIdle = _isDance = _isMove = false;
                 }
                 _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
             }
@@ -66,18 +59,20 @@ namespace _Bootcamp.Scripts.Player
             _playerVelocity.y += _gravityValue * Time.deltaTime;
             _characterController.Move(_playerVelocity * Time.deltaTime);
             
+            _groundedPlayer = _characterController.isGrounded;
+
             if (moveInput == Vector2.zero)
             {
-                if (!_isIdle && (_isMove || _isRun) && !_isJump)
+                if (!_isIdle && _groundedPlayer && !_isDance)
                 {
-                    _anim.CrossFade("Idle", .2f);
+                    _anim.CrossFade("Idle", .1f);
                     _isIdle = true;
+                    _isMove = _isRun = _isJump = false;
                 }
-                _isMove = _isRun = false;
                 return;
             }
 
-            if (moveInput != Vector2.zero && !_isJump)
+            if (moveInput != Vector2.zero && _groundedPlayer)
             {
                 _anim.SetFloat("WalkSpeed", moveInput.y < 0 ? -1 : 1);
 
@@ -85,17 +80,30 @@ namespace _Bootcamp.Scripts.Player
                 {
                     _isRun = true;
                     _anim.CrossFade("Run", .1f);
-                    _isMove = false;
+                    _isMove = _isIdle = _isJump = _isDance = false;
                 }
                 else if (!sprinted && !_isMove)
                 {
                     _isMove = true;
-                    _anim.CrossFade("Walk", .1f);
-                    _isRun = false;
+                    _anim.CrossFade("Walk", .08f);
+                    _isRun = _isIdle = _isJump = _isDance = false;
                 }
                 
                 _isIdle = false;
             }
+        }
+
+        private void Actions()
+        {
+            if (_playerInput.Danced1ThisFrame())
+                _anim.CrossFade("Dance1", .1f);
+            else if (_playerInput.Danced2ThisFrame())
+                _anim.CrossFade("Dance2", .1f);
+            else 
+                return;
+            
+            _isMove = _isRun = _isIdle =  false;
+            _isDance = true;
         }
     }
 }
