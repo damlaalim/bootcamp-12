298ab7c87c953956a83e8ffd05a6aba6aae05fbd
damlaalim <damlaalim81@gmail.com>
2024-08-02 02:44:52 +0300

    tutorial bug fix

--- Assets/_Bootcamp/Scripts/Traps/DeadSpawner.cs
+++ Assets/_Bootcamp/Scripts/Traps/DeadSpawner.cs
@@ -1,41 +1,51 @@
 ﻿using Photon.Pun;
 using UnityEngine;
 
-public class DeadSpawner : MonoBehaviourPun
+public class DeadSpawner : MonoBehaviour
 {
     Vector3 leftPosition = new Vector3(1.45129848f, 15.4154911f, 23.6990147f);
     Vector3 rightPosition = new Vector3(-13.82f, 6.3f, 13.43f);
 
-    private void OnTriggerEnter(Collider other)
+    public Vector3 GetPosition()
     {
-        if (other.CompareTag("Player"))
+        if (PhotonNetwork.IsMasterClient)
         {
-            Vector3 spawnPosition;
-            if (PhotonNetwork.IsMasterClient)
-            {
-                Debug.Log("Right Position");
-                spawnPosition = rightPosition;
-            }
-            else
-            {
-                Debug.Log("Left Position");
-                spawnPosition = leftPosition;
-            }
-
-            PhotonView otherPhotonView = other.GetComponent<PhotonView>();
-            if (otherPhotonView != null && otherPhotonView.IsMine)
-            {
-                // Karakterin yeni pozisyona taşınması
-                otherPhotonView.RPC("Teleport", RpcTarget.All, other.transform,spawnPosition);
-            }
-
+            return rightPosition;
         }
-
+        else
+            return leftPosition;
     }
-    [PunRPC]
-    private void Teleport(Transform transform, Vector3 spawnPosition)
-    {
-        transform.transform.position = spawnPosition;
-
-    }
+    
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         Vector3 spawnPosition;
+    //         if (PhotonNetwork.IsMasterClient)
+    //         {
+    //             Debug.Log("Right Position");
+    //             spawnPosition = rightPosition;
+    //         }
+    //         else
+    //         {
+    //             Debug.Log("Left Position");
+    //             spawnPosition = leftPosition;
+    //         }
+    //
+    //         PhotonView otherPhotonView = other.GetComponent<PhotonView>();
+    //         if (otherPhotonView != null && otherPhotonView.IsMine)
+    //         {
+    //             // Karakterin yeni pozisyona taşınması
+    //             otherPhotonView.RPC("Teleport", RpcTarget.All, other.transform,spawnPosition);
+    //         }
+    //
+    //     }
+    //
+    // }
+    // [PunRPC]
+    // private void Teleport(Transform transform, Vector3 spawnPosition)
+    // {
+    //     transform.transform.position = spawnPosition;
+    //
+    // }
 }
