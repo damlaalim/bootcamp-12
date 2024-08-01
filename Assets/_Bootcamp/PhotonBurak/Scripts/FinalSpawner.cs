6780cd4aa2cdf3d6101bae43e9bc49e31f8ada1a
Burak Arslan <Burakarslan789@gmail.com>
2024-08-02 01:10:47 +0300

    Final scene spawner

--- Assets/_Bootcamp/PhotonBurak/Scripts/FinalSpawner.cs
+++ Assets/_Bootcamp/PhotonBurak/Scripts/FinalSpawner.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 namespace _Bootcamp.PhotonBurak.Scripts
 {
-    public class Puzzle_2Spawner: MonoBehaviourPun
+    public class FinalSpawner:MonoBehaviourPun
     {
         void Start()
         {
-            Vector3 leftPosition = new Vector3(0f,0.06f,-7.15f);
-            Vector3 rightPosition = new Vector3(11.17f, 0.06f, -7.15f);
+            Vector3 leftPosition = new Vector3(8.54f,5.02f,40.11f);
+            Vector3 rightPosition = new Vector3(17.28f, 5.02f, 40.11f);
 
             Vector3 spawnPosition;
             if (PhotonNetwork.IsMasterClient)
