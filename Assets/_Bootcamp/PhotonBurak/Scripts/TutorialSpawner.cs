535a5ca17e6583aab5b40a431254707012777885
bugra <bugra.ksr@gmail.com>
2024-08-02 01:23:56 +0300

    Tutorial mini fix 2

--- Assets/_Bootcamp/PhotonBurak/Scripts/TutorialSpawner.cs
+++ Assets/_Bootcamp/PhotonBurak/Scripts/TutorialSpawner.cs
@@ -6,7 +6,7 @@ public class TutorialSpawner : MonoBehaviourPun
 {
     void Start()
     {
-        Vector3 leftPosition = new Vector3(+62f, 80f, 198f);
+        Vector3 leftPosition = new Vector3(1.45129848f, 15.4154911f, 23.6990147f);
         Vector3 rightPosition = new Vector3(-13.82f, 6.3f, 13.43f);
 
         Vector3 spawnPosition;
