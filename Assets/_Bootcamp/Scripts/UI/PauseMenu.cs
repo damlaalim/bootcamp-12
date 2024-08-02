f792335d7ba8a81b81300b59c50dd15512ed611a
LariXv1 <berkaysagdic@hotmail.com>
2024-08-02 21:18:19 +0300

    Pause screen ayar

--- Assets/_Bootcamp/Scripts/UI/PauseMenu.cs
+++ Assets/_Bootcamp/Scripts/UI/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -29,7 +30,7 @@ public class PauseMenu : MonoBehaviour
 
     public void MainMenu()
     {
-        //Main menuyu y�kle
+        SceneManager.LoadScene("LoadingPhoton");
     }
 
     public void CloseMenu()
