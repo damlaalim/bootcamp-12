4fd8f02332f24f8c35db5b4cb9c08ebb8971c3a8
Burak Arslan <Burakarslan789@gmail.com>
2024-08-02 19:43:35 +0300

    playerchatupdate

--- Assets/_Bootcamp/PhotonBurak/Scripts/CanvasTrigger.cs
+++ Assets/_Bootcamp/PhotonBurak/Scripts/CanvasTrigger.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 namespace _Bootcamp.PhotonBurak.Scripts
 {
-    public class CanvasTrigger : MonoBehaviourPun
+    public class CanvasTrigger : MonoBehaviourPunCallbacks
     {
         [SerializeField] private PhotonCharacterController _character;
         public Canvas Canvas;
 
         private void Start()
         {
-            if (photonView.IsMine && Canvas != null)
+            if (Canvas != null)
             {
                 Canvas.enabled = false;
             }
@@ -18,27 +18,12 @@ namespace _Bootcamp.PhotonBurak.Scripts
 
         private void Update()
         {
-            if (photonView.IsMine && Input.GetKeyDown(KeyCode.K) && Canvas != null)
+            if (Input.GetKeyDown(KeyCode.K) && Canvas != null)
             {
                 var enabled = !Canvas.enabled;
                 Canvas.enabled = enabled;
                 _character.canMove = !enabled;
                 _character.openCanvas = enabled; 
-                Cursor.lockState = !enabled ? CursorLockMode.Locked : CursorLockMode.None;
-
-                // Canvas durumunu diğer oyunculara bildir
-                photonView.RPC("SyncCanvasState", RpcTarget.Others, enabled);
-            }
-        }
-
-        [PunRPC]
-        private void SyncCanvasState(bool enabled)
-        {
-            if (!photonView.IsMine && Canvas != null)
-            {
-                Canvas.enabled = enabled;
-                _character.canMove = !enabled;
-                _character.openCanvas = enabled;
                 Cursor.lockState = !enabled ? CursorLockMode.Locked : CursorLockMode.None;
             }
         }
