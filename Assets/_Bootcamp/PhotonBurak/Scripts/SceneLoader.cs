713ad81b6f859b1b85d6440e886fc036afea94b5
damlaalim <damlaalim81@gmail.com>
2024-08-08 16:05:08 +0300

    sunum için sahnelerin tek tuş ile değiştirilmesi eklendi

--- Assets/_Bootcamp/PhotonBurak/Scripts/SceneLoader.cs
+++ Assets/_Bootcamp/PhotonBurak/Scripts/SceneLoader.cs
@@ -1,15 +1,34 @@
+using System;
+using System.Collections.Generic;
 using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _Bootcamp.PhotonBurak.Scripts
 {
     public class SceneLoader : MonoBehaviourPunCallbacks
     {
+        public bool load;
+        public List<string> levelList; 
+        
         public void ChangeScene()
         {
             if (PhotonNetwork.IsMasterClient)
             {
                 PhotonNetwork.LoadLevel("Office");
             }
+        }
+
+        private void Update()
+        {
+            if (!load || !Input.GetKeyDown(KeyCode.P) || !PhotonNetwork.IsMasterClient)
+                return;
+            var current = levelList.FindIndex(x => 
+                x == SceneManager.GetActiveScene().name) + 1;
+            if (current >= levelList.Count)
+                current = 0;
+            
+            PhotonNetwork.LoadLevel(levelList[current]);
         }
     }
 }
